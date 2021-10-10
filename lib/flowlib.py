96665feceec66286c0f24591f93aeb454af38045
Siavash Khallaghi <khallagh65@gmail.com>
2021-10-09 17:13:49 -0700

    Making things work.

--- lib/flowlib.py
+++ lib/flowlib.py
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 import png
-import pfm
+from . import pfm
 import numpy as np
 import matplotlib.colors as cl
 import matplotlib.pyplot as plt
